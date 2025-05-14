94ccbdfaecd5d45ab2868407f5f01589aecf9851
Somya Bhatt <somyacu1@gmail.com>
2025-05-14 11:37:37 +0530

    Update main.tsx

--- react/react-todo/src/main.tsx
+++ react/react-todo/src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Demo from './use-reducer-demo.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  // <App />
+  <Demo />
 )
